1611224b66e2e9d019de3c2f36e3127f84230ab8
Rahul Sunil <rahulsunil2@gmail.com>
2021-03-28 16:34:30 +0530

    Angular Complete

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = [];
+  private PostsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(){
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener()
+      .subscribe(
+        (posts: Post[]) => {
+          this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(){
+    this.PostsSub.unsubscribe();
+  }
 
 }
