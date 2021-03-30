9698ae03ca082daed131df5d67a07ef55b8f88ac
Rahul Sunil <rahulsunil2@gmail.com>
2021-03-30 10:09:36 +0530

    Database Added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
+
+mongoose.connect("mongodb+srv://nodejs:VNFRaEbYdDLlDIzO@hellomongodb.a6yzc.mongodb.net/node-angular?retryWrites=true&w=majority")
+    .then(() => {
+        console.log("Connected to Db!");
+    })
+    .catch(() => {
+        console.log("Connection Failed!");
+    })
 
 const app = express();
 
@@ -14,29 +25,32 @@ app.use((req, res, next) => {
 })
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-        message: "Post Added Successfully!"
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: "Post Added Successfully!",
+            postId: createdPost._id
+        });
     });
 })
 
 app.get("/api/posts", (req, res, next) => {
-    const posts = [{
-            id: "001",
-            title: "First Post",
-            content: "First Post Content"
-        },
-        {
-            id: "002",
-            title: "Second Post",
-            content: "Second Post Content"
-        }
-    ]
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: "Posts Fetched!",
+            posts: documents
+        });
+    });
 
-    res.status(200).json({
-        message: "Posts Fetched!",
-        posts: posts
+})
+
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({ _id: req.params.id }).then(result => {
+        console.log(result);
+        res.status(200).json({ message: "Post Deleted!" });
     })
 })
 
